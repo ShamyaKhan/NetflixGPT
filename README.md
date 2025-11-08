7502991c8639d40b52db200c8adb8dd05240f9be
Shamya Khan <md.shamiulhaque127@gmail.com>
2025-11-08 16:44:47 +0600

    TMDB API data fetch && built Main Container

--- README.md
+++ README.md
@@ -15,6 +15,21 @@
 - Created redux store with userSlice
 - Implemented Sign Out
 - Update profile API call
+- Bug fix: Signup user display name and profile picture update
+- Bug fix: redirect to /browse for logged in users
+- Unsubscribe to the onAuthStateChanged callback
+- Add hardcode values to constant file
+- Register TMDB API, create an app, get access token
+- Get data from TMDB now playing movies list API
+- Custom hook for now playing movies
+- Create a movie slice
+- Update store with movies data
+- PLanning for main container and secondary container
+- Fetch data for trailer video
+- Update store with trailer video
+- Embedded youtube video and make it autoplay and mute
+- Tailwind classes to make main container look awesome
+-
 
 # Features
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,14 +7,13 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const email = useRef(null);
@@ -36,7 +35,7 @@ const Login = () => {
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "https://avatars.githubusercontent.com/u/111437672?v=4",
+            photoURL: USER_AVATAR,
           })
             .then(() => {
               const { uid, email, displayName, photoURL } = auth.currentUser;
@@ -48,12 +47,10 @@ const Login = () => {
                   photoURL: photoURL,
                 })
               );
-              navigate("/browse");
             })
             .catch((error) => {
               setErrorMessage(error.message);
             });
-          console.log(user);
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -68,8 +65,6 @@ const Login = () => {
       )
         .then((userCredential) => {
           const user = userCredential.user;
-          console.log(user);
-          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
