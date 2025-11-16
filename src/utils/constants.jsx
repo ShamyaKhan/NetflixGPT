750b3c6d01ee1c04156a7a91d987330a3eb535dd
Shamya Khan <md.shamiulhaque127@gmail.com>
2025-11-16 03:33:05 -0600

    Corrected API key constants

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -8,7 +8,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+    Authorization: "Bearer " + import.meta.env.VITE_TMDB_KEY,
   },
 };
 
@@ -23,6 +23,6 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
-export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+export const OPENAI_KEY = import.meta.env.VITE_OPENAI_KEY;
 
-export const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+export const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
